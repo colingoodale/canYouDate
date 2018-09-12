1904b4b0937c531309ebcb6618496a150e63eabd
Colin <colingoodale@gmail.com>
2018-09-11 19:29:19 -0500

    finished data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,34 @@
 var availableFriends = [
     {
-        test: "test",
+        name: "Adolph",
+        photo: "https://randomuser.me/api/portraits/men/0.jpg",
+        scores: [5, 3, 4, 4, 3, 2, 1, 2, 1, 3]
     },
-    {}]
+    {
+        name: "Rudolph",
+        photo: "https://randomuser.me/api/portraits/men/88.jpg",
+        scores: [1, 2, 3, 2, 1, 4, 5, 4, 5, 4]
+    },
+    {
+        name: "Katherine",
+        photo: "https://randomuser.me/api/portraits/women/18.jpg",
+        scores: [3, 2, 4, 5, 2, 3, 1, 4, 5, 4]
+    },
+    {
+        name: "Anabelle",
+        photo: "https://randomuser.me/api/portraits/women/74.jpg",
+        scores: [5, 1, 3, 2, 4, 3, 1, 5, 3, 2]
+    },
+    {
+        name: "Eric",
+        photo: "https://randomuser.me/api/portraits/men/45.jpg",
+        scores: [5, 4, 5, 3, 4, 2, 1, 2, 3, 5]
+    },
+    {
+        name: "Elise",
+        photo: "https://randomuser.me/api/portraits/women/19.jpg",
+        scores: [1, 2, 3, 2, 3, 1, 2, 3, 2, 1]
+    }
+]
 
 module.exports = availableFriends;
